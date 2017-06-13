8cfc082656dbc0e4e1a3f509ac61426f975eef15
Jeff Cohen <jeffcoh23@gmail.com>
2017-06-13 18:56:10 -0400

    new lib file

---
+++ lib/data_migrate_with_stats.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__), 'data_migrate_with_stats', 'data_migrator')
+require File.join(File.dirname(__FILE__), 'data_migrate_with_stats', 'data_schema_migration')
+require File.join(File.dirname(__FILE__), 'data_migrate_with_stats', 'migration')
+require File.join(File.dirname(__FILE__), 'data_migrate_with_stats', 'railtie')
