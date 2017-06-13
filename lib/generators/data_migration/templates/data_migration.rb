0a6bab8e674d1b39977074c5a0e518fe2e758cf1
Jeff Cohen <jeffcoh23@gmail.com>
2017-06-13 19:06:00 -0400

    tests

--- lib/generators/data_migration/templates/data_migration.rb
+++ lib/generators/data_migration/templates/data_migration.rb
@@ -1,5 +1,10 @@
+require_relative './helpers'
 class <%= migration_class_name %> < ActiveRecord::Migration
   def self.up
+    print_memory_usage do
+      print_time_spent do
+      end
+    end
   end
 
   def self.down
