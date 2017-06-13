72283847e4d6f619429191b5816dead01a01e3a1
Jeff Cohen <jeffcoh23@gmail.com>
2017-06-13 18:07:58 -0400

    class name change

--- lib/generators/data_migration/data_migration_generator.rb
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators/migration'
 
 module DataMigrate
   module Generators
-    class DataMigrationGenerator < Rails::Generators::NamedBase
+    class DataMigrationWithStatsGenerator < Rails::Generators::NamedBase
       namespace "data_migration"
       include Rails::Generators::Migration
 
