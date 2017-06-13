80edafc165cb33e5bc3e26e7279adbe3a23d167f
Jeff Cohen <jeffcoh23@gmail.com>
2017-06-13 18:16:49 -0400

    again

--- lib/generators/data_migrate.rb
+++ lib/generators/data_migrate.rb
@@ -1,9 +1,11 @@
 require 'rails/generators/named_base'
 module DataMigrate
   module Generators
-    class DataMigrationGenerator < Rails::Generators::NamedBase #:nodoc:
-      def self.source_root
-         @_data_migrate_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates'))
+    class DataMigrationGenerator < Rails::Generators::NamedBase
+      class DataMigrationWithStatsGenerator < Rails::Generators::NamedBase #:nodoc:
+        def self.source_root
+           @_data_migrate_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates'))
+        end
       end
     end
   end
